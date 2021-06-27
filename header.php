b122c2c5d95667df50576712efd5c61d46736ed7
Jotaele13507 <jorge.sanchezr@up.ac.pa>
2021-06-27 00:35:50 -0500

    a

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
-<html>
+<html lang="es">
 	<head>
+	<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
 		<link rel="stylesheet" type="text/css" href="css/header_style.css" />
 	</head>
@@ -8,7 +9,7 @@
 			<a href="../index.php">
 				<div id="cd-logo">
 					<img src="img/ic_logo.svg" alt="Logo" />
-					<p>LIBRERÍA CONFIGUROWEB</p>
+					<p>LIBRERÍA CSD</p>
 				</div>
 			</a>
 		</header>
